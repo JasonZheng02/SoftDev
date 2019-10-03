e81411ee0a747683bbaf7123f1a622eeed9df5c8
JasonZheng02 <heydude506@gmail.com>
2019-10-03 14:56:26 -0400

    Work

--- 15_login/flaskApp.py
+++ 15_login/flaskApp.py
@@ -3,16 +3,19 @@
 # K
 # Date
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def login():
 	return render_template("login.html", foo = "Login Page")
 
-@app.route("/foo.html")
-def html():
-	return render_template("Static.html", foo = "title")
+@app.route("/Welcome")
+def welcome():
+	return render_template("welcome.html",
+							foo = "Welcome Page",
+							user = request.args['username'],
+							pword = request.args['password'])
 
 if __name__ == "__main__":
 	app.debug = True
