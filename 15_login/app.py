53d48b059847ba126f01ba13a78a2681d9df2c0e
Jason Zheng <jzheng02@stuy.edu>
2019-10-08 02:06:14 -0400

    Done

---
+++ 15_login/app.py
@@ -0,0 +1,27 @@
+# Jason Zheng
+# SoftDev1 pd09
+# K
+# Date
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+	return render_template("login.html")
+
+@app.route("/Welcome")
+def welcome():
+    if request.args['username'] == "Bob" and request.args['password'] == "Green Cheese":
+        return render_template("welcome.html",
+                                user = request.args['username'])
+    elif request.args['username'] != "Bob":
+        return render_template("failuser.html",
+                                user = request.args['username'])
+    else:
+        return render_template("failpword.html",
+                                user = request.args['username'])
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
