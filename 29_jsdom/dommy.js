8bbff3338e2112be0b2d7c41fc5d42e95269ba27
Jason Zheng <jzheng02@stuy.edu>
2019-12-12 23:34:36 -0500

    .

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -17,9 +17,9 @@ for (var i = 0; i < lis.length; i++) {
 
 var addItem = function() {
   var list = document.getElementById("thelist");
-  var ele = document.createElement("li");
-  ele.innerHTML = "WORD";
-  list.append(ele);
+  var item = document.createElement("li");
+  item.innerHTML = "WORD";
+  list.append(item);
   ele.addEventListener('click', function(e){removeItem(this), console.log(e);});
   ele.addEventListener('mouseover', function(){changeHeading(this.innerHTML)});
   ele.addEventListener('mouseout', function(){changeHeading("Hello World!")});
@@ -27,3 +27,24 @@ var addItem = function() {
 
 var button = document.getElementById("b");
 button.addEventListener('click', addItem);
+
+var fib = function(n) {
+	if ( n < 2 ){
+		return 1;
+	}
+	else{
+		return fib(n-1) + fib(n-2);
+	}
+};
+
+var addFib = function(e) {
+	console.log(e);
+	var list = document.getElementById("fiblist");
+	var len = list.getElementsByTagName("li").length;
+	var item = document.createElement("li");
+	item.innerHTML = fib(len);
+	list.append(item);
+}
+
+var fb = document.getElementById("fb");
+fb.addEventListener("click", addFib);
