d5bc52338c4eb3014cffaaf71cb7f60f48d21ac4
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 23:00:37 -0500

    Done

--- spring/06_canvas/dotconn.js
+++ spring/06_canvas/dotconn.js
@@ -1,25 +1,23 @@
 /*
 Jason Zheng
 SoftDev Pd. 2
-K #05: ...and I want to Paint It Better
-2020-02-06
+K #06: Dot Dot Dot
+2020-02-11
 */
 
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
+var prevX, prevY;
 
 // Clears Canvas
 var clean = function() {
   ctx.clearRect(0, 0, c.width, c.height);
+  prevX = null;
+  prevY = null;
 }
 
 // Creates a dot at mouse location
-var dot = function(){
-  ctx.beginPath();
-  ctx.moveTo(0,0);
-  ctx.lineTo(event.clientX, event.clientY);
-  ctx.stroke();
-  ctx.closePath();
+var dot = function() {
   ctx.beginPath();
   ctx.arc(event.clientX, event.clientY, 10, 0, 2 * Math.PI);
   ctx.fill();
@@ -27,9 +25,23 @@ var dot = function(){
   ctx.closePath();
 }
 
+// Creates a line segment from one dot to another
+var line = function(){
+  ctx.beginPath();
+	ctx.moveTo(prevX,prevY);
+	ctx.lineTo(event.clientX, event.clientY);
+	ctx.stroke();
+  ctx.closePath();
+  prevX = event.clientX;
+  prevY = event.clientY;
+}
+
 // On click run clear function
 var clear = document.getElementById("clear");
 clear.addEventListener("click", clean);
 
 // On click run draw function
-c.addEventListener("click", dot)
+c.addEventListener("click", function(e){
+  dot();
+  line();
+});
