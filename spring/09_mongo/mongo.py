2e8dd3e5b2482a361d06f127339cae7a0b1decc6
Jason Zheng <jzheng02@stuy.edu>
2020-02-27 02:49:36 -0500

    .

--- spring/09_mongo/mongo.py
+++ spring/09_mongo/mongo.py
@@ -1,5 +1,15 @@
-import pymongo
+from pymongo import MongoClient
 
-restaurants = open("primer-dataset.json", "r")
-str = restaurants.readlines(1)
-print(str)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["database"]
+col = db["restaurants"]
+
+file = open("primer-dataset.json", "r")
+content = file.readlines()
+for line in content:
+    col.insert_one(line)
+
+#getting this error
+#document must be an instance of dict, bson.son.SON,
+#bson.raw_bson.RawBSONDocument, or a type that inherits
+#from collections.MutableMapping
